00794c00485aba5c514431c18535d732fd967f7e
Jared Schau <jaredschau@MacBook-Pro.local>
2020-06-30 21:05:12 -0600

    basic html css and js questions

---
+++ questions.js
@@ -0,0 +1,57 @@
+var questions = [
+    {
+      title: "Which method would you use to find an ID element?",
+      choices: ["getElementsById()", "getElementByID()", "getElementbyId()", "getElementsById()"],
+      answer: "getElementbyId()"
+    },
+    {
+      title: "To see if two variables are equal in an if / else statement you would use ____.",
+      choices: ["=", "==", "'equals'", "!="],
+      answer: "=="
+    },
+    {
+      title: "Math.random() returns ____.",
+      choices: ["a number between 1 and 9", "a number between 0 and 9", "a number between 0 and 1", "a number between 0 and 99"],
+      answer: "a number between 0 and 1"
+    }, 
+    {
+      title: "The appendChild() method places a node as the ____ child.",
+      choices: ["first", "last place you left off", "random", "last"],
+      answer: "last"
+    }, 
+    {
+        title: "An array always begins at an index of:",
+        choices: ["-1", "0", "1", "null"],
+        answer: "0"
+    },
+    {
+        title: "A function within an object is called a ____.",
+        choices: ["property", "key", "value", "method"],
+        answer: "method"
+    },
+    {
+        title: "The difference between the == and === operators does not involve:",
+        choices: ["strict comparison", "assignment", "type conversion", "abstract comparison"],
+        answer: "assignment"
+    },
+    {
+        title: "Which of the following statements does not create a loop?",
+        choices: ["switch", "for", "while", "do...while"],
+        answer: "switch"
+    },
+    {
+        title: "Which of the following is not a way to declare a variable?",
+        choices: ["var", "set", "let", "const"],
+        answer: "set"
+    },
+    {
+        title: "Which of the following functions is not dependent on the user?",
+        choices: ["alert", "prompt", "console.log()", "confirm"],
+        answer: "console.log()"
+    },
+    {
+      title: "Javascript variables can be ______ ",
+      choices: ["globally scoped", "function scoped", "locally scoped", "all of the above"],
+      answer: "all of the above"
+    }, 
+  ];
